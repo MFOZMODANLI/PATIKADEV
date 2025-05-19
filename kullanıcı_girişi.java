4aa003df3e43f7939e35bc5f04676eecede1ca11
Mehmet Fatih Özmodanlı <fatihozmodanli@gmail.com>
2025-05-19 15:54:04 +0300

    kullanıcı giriş

---
+++ kullanıcı_girişi.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class kullanıcı_girişi {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+		String userNameExpected = "fatih";
+		String passwordExpected = "fatih123";
+
+		System.out.print("Kullanıcı Adınızı Giriniz : ");
+		String userName = scanner.nextLine();
+
+		System.out.print("Şifrenizi Giriniz : ");
+		String password = scanner.nextLine();
+
+		if (userName.equals(userNameExpected) && password.equals(passwordExpected)) {
+			System.out.println("Giriş Yaptınız.");
+		} else {
+            System.out.println("Bilgileriniz yanlış...");
+		}
+        scanner.close();s
+    }
+    
+}
